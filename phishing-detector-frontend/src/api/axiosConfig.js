5a502350b337c695cff74d940c96340e9bb0ec36
Rossyyyyyy <163552050+Rossyyyyyy@users.noreply.github.com>
2024-11-17 14:06:45 +0800

    Update axiosConfig.js

--- phishing-detector-frontend/src/api/axiosConfig.js
+++ phishing-detector-frontend/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://finalemail.onrender.com'; // Ensure this is your backend's API base URL
+const API_BASE_URL = 'https://finalemail.onrender.com/api';
+
 
 
 export const fetchUserInfo = async (token) => {
